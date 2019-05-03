4e96a3c622c3c615cb0cfb2c0586bf4bee62efde
Luis Gustavo de Barros Lima <c4pote@gmail.com>
2019-05-03 13:34:19 -0300

    V3

--- main.py
+++ main.py
@@ -1,27 +1,58 @@
 # -*- coding: utf-8 -*-
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import io
+
+#IMPORT UTILIZADO NO SLEEP
 import time
+
+#Reportlab Básico
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
+
+#Polegadas e Centimetros para responsividade.
 from reportlab.lib.units import inch
+from reportlab.lib.units import cm
+
+#Cores para habilitar transparencia
+from reportlab.lib.colors import PCMYKColor, PCMYKColorSep, Color
+
+#trabalhando com imagem para adicionar o logo da jatinox
+from reportlab.lib.utils import ImageReader
+from reportlab.lib import utils
+from reportlab.platypus import Frame, Image
+
 import os
-#import shutil
+
+
+path = 'B:\\MATRIZ\\CERTIFICADOS'
+#path = 'C:\\Users\\Lucas.Oliveira\\Documents\\CarimbaCertificadoPython'
+#path = 'C:\\Users\\gustavo.lima\\Desktop\\CERTIFICADOS'
 
 debug=False
+'''
+#DESATIVADO
+#logo = ImageReader('C:\\Users\\gustavo.lima\\Desktop\\CERTIFICADOS\\logo.png')
 
+#Redimensiona Imagem
+def get_image(path, width=1*cm):
+	img = utils.ImageReader(path)
+	iw, ih = img.getSize()
+	aspect = ih / float(iw)
+	return Image(path, width=width, height=(width * aspect))
+'''
 
-def tratarlote(lotesemtratar):
-	lotetratado = lotesemtratar.replace('-CARTA','')
-	lotetratado = lotetratado.replace('CARTA','')
+#para tratar o lote deixando ele visualmente melhor e removendo informacoes que nao utilizamos para lote.
+def tratalote(lotesemtratar):
+	lotetratado= lotesemtratar.replace('-CARTA','')
+	lotetratado= lotetratado.replace('CARTA','')# exemplos para outros casos
+	lotetratado= lotetratado.replace(' ','')#todos os espaços existentes, principalmente os casos "BN  
+	lotetratado= str(lotetratado[:2]) + ' ' + str(lotetratado[2:])# Coloca um espaco entre Tipo e Lote
 	return lotetratado
-#Obtendo o Sistema Operacional
-#import platform
-#so = platform.system()
+
 
 def moverarquivo(arquivoorigem):
 	#MOVENDO ARQUIVO DE ORIGEM
-	arquivo= (os.path.split(arquivoorigem))
+	arquivo = (os.path.split(arquivoorigem))
 	diretorio= arquivo[0]
 	arquivo= (os.path.splitext(arquivo[1]))
 	lote= arquivo[0]
@@ -35,35 +66,28 @@ def moverarquivo(arquivoorigem):
 			if debug:
 				print ('Pasta criada com sucesso!')
 			os.mkdir(pasta)
-		#os.rename(arquivoorigem,"ORIGINAL/"+arquivoorigem)
 		
 	except:
 		if debug:
 			print("arquivoscarimbar")
 
 
-
-	#if so == 'Linux':
-	#		os.rename(arquivoorigem,"ORIGINAL/"+arquivoorigem)
-	#elif so == 'Windows':
-	#	if debug:
-	#		print ('Windows')
 	arquivodestino = os.path.join(diretorio,"ORIGINAL",lote) +".pdf" 
 	print (arquivodestino)
+
+	#AQUI VOCE DEVE CRIAR A VERIFICACAO DE EXISTENCIA DO ARQUIVO DESTINO, CASO O MESMO EXISTA REMOVER ANTES DA PROXIMA FUNCAO OS.RENAME.
+	if os.path.isfile(arquivodestino):
+		os.remove(arquivodestino)
+		
 	os.rename(arquivoorigem,arquivodestino)
-		#shutil.move(arquivoorigem,arquivoscarimbar)
-		#shutil.copy("C:\\Users\\Lucas.Oliveira\\Documents\\CarimbaCertificadoPython\\BN\\18\\24XX\\","C:\\Users\\Lucas.Oliveira\\Documents\\CarimbaCertificadoPython\\BN\\18\\24XX\\ORIGINAL\\"+arquivoorigem)
-	#else:
-	#	return 'Sistema Operacional sem suporte. Host: ' + platform.node() + ' Sistema: ' + so
-	#return 'Movido com Sucesso!'
 
 
 def carimbaPDF(arquivoorigem,line1,line2):
-	arquivo= (os.path.split(arquivoorigem))
-	#print(os.path.splitext(abc[1]))
+	arquivo = (os.path.split(arquivoorigem)) #Ex arquivoorigem = c:\certificado\BN\arquivo.pdf entao arquivo vai receber no primeiro array 'c:\certificado\BN\' e no segundo 'arquivo.pdf'
 	diretorio= arquivo[0]
-	arquivo= (os.path.splitext(arquivo[1]))
-	lote= arquivo[0]
+	arquivo = (os.path.splitext(arquivo[1])) #Utilizando o mesmo ex acima aqui receber no primeiro array 'arquivo' e no segundo '.pdf'
+	lote = arquivo[0] #Carregamos o nome do arquivo que será carimbado no PDF do certificado.
+	
 	packet = io.BytesIO()
 	# Criando um novo PDF com Reportlab
 	can = canvas.Canvas(packet, pagesize=A4)
@@ -71,28 +95,52 @@ def carimbaPDF(arquivoorigem,line1,line2):
 	can.translate(inch,inch)
 	# Define fonte e largura
 	can.setFont("Helvetica", 15)
-	# Mudar cor
+	# Mudar cor da Moldura para Perto
 	can.setStrokeColorRGB(0,0,0)
-	can.setFillColorRGB(1,0,1)
+	can.setFillColor(Color(255,255,255, alpha=0.3))
+	#can.setFillColorRGB(1,0,1)
 
 	# Desenhando linha que separa o lote das observacoes
-	can.line(-0.66*inch,-0.69*inch,-0.66*inch,0.8*inch)
+	# can.line(-0.66*inch,-0.69*inch,-0.66*inch,0.8*inch)
 
 	# Desenhando Retangulo para parecer um carimbo
-	can.rect(-0.9*inch,-0.7*inch,0.7*inch,1.5*inch, fill=0)
+	#can.rect(-0.94*inch,-0.7*inch,0.6*inch,1.5*inch, fill=1)
+	# CASO NAO TENHA A INFORMACAO NA LINHA 2 O RETANGULO VAI SER REDUZIDO.
+	if line2 == "":
+		can.rect(-0.94*inch,-0.7*inch,0.4*inch,1.5*inch, fill=1)
+	else:
+		can.rect(-0.94*inch,-0.7*inch,0.6*inch,1.5*inch, fill=1)
+
 	# Rotacionando carimbo para utilizar a margem e nao sobrepor muito os dados do documento
 	can.rotate(90)
-	# Mudando a Cor do texto para Azul
-	can.setFillColorRGB(0,0,0.77)
-	# Dados do Carimbo
-	can.drawString(-0.63*inch,  0.7 *inch,  '{: >11}'.format(tratarlote(lote)))
-	can.drawString(-0.66*inch,  0.46 *inch, '{: >14}'.format(line1))
-	can.drawString(-0.66*inch,  0.26 *inch, '{: >14}'.format(line2))
-
+	# Mudando a Cor do texto para Azul e removendo transparencia.
+	can.setFillColor(Color(0,0,0.77, alpha=1))
 	
+	# Dados do Carimbo
+	can.drawString(-0.60*inch,  0.76 *inch,  '{: >12}'.format(tratalote(lote)))
+	can.drawString(-0.78*inch,  0.58 *inch, '{: >14}'.format(line1))
+	can.drawString(-0.68*inch,  0.38 *inch, '{: >14}'.format(line2))
+
+
+	'''
+	DESABILITADO LOGOTIPO POIS SEGUNDO O RAFAEL E RAUL NAO EXISTE GANHO PARA MARCA COLOCAR O LOGO NO CERTIFICADO
+	PELO CONTRARIO ALGUMA REVENDA PODE PEGAR O CERTIFICADO E USAR PARA ENTREGAR PARA ALGUM CLIENTE QUE VENHA TER PROBLEMA E AI VAO RELACIONAR O 
+	PROBLEMA A MARCA JATINOX. O CODIGO VAI CONTINUAR PARA EXEMPLO MAS POSSIVELMENTE NAO VAI TER LOGO
+	'''
+
+	#ADD LOGO JATINOX
+	''''
+	frame = Frame(-0.96*inch, -1.26 *inch, 5*cm, 5*cm, showBoundary=0)
+	story = []
+	#Redimensionando Imagem do LOGO JATINOX para 2cm mantendo proporção
+	story.append(get_image('logo.png', width=1.4*cm))
+	frame.addFromList(story, can)
+	#can.drawImage(logo, -0.66*inch, -1.3 *inch, mask='auto')
+	'''
+
     #Adicionando Autoria e Titulo
-	can.setAuthor("JATINOX")
-	can.setTitle("CERTIFICADO JATINOX")
+	#can.setAuthor("JATINOX")
+	#can.setTitle("CERTIFICADO JATINOX")
 
 
 	can.save() 
@@ -100,6 +148,7 @@ def carimbaPDF(arquivoorigem,line1,line2):
 	#mover para o inicio o buffer StringIO
 	packet.seek(0)
 	new_pdf = PdfFileReader(packet)
+	
 	# Leitura do PDF de ORIGEM
 	f=open(arquivoorigem, "rb")
 	existing_pdf = PdfFileReader(f)
@@ -115,13 +164,12 @@ def carimbaPDF(arquivoorigem,line1,line2):
 	paginas = existing_pdf.getNumPages()
 	# Copiando e carimbando todas as paginas.
 	for numero_pagina in range(paginas):
-		page = existing_pdf.getPage(numero_pagina)
-		page.mergePage(new_pdf.getPage(0))
-		output.addPage(page)
-	# Escrevendo o arquivo de destino
+		page = existing_pdf.getPage(numero_pagina) #Cria uma pagina clone do arquivo origem
+		page.mergePage(new_pdf.getPage(0)) #Adiciona o carimbo criado no canvas na nova pagina criada.
+		output.addPage(page) #Salva a pagina carimbada na variavel de output
 	
-	#outputStream = open(lote + "-C.pdf", "wb")
-	tempor= diretorio + "\\" + lote + "-C.pdf"
+	# Escrevendo o arquivo de destino
+	tempor = diretorio + "\\" + lote + "-C.pdf" # tempor = variável temporaria
 	outputStream = open(tempor, "wb")
 	if debug:
 		print(tempor)
@@ -130,44 +178,39 @@ def carimbaPDF(arquivoorigem,line1,line2):
 
 
 	f.close()
-	#moverarquivo(arquivoorigem)
-path = 'B:\\MATRIZ\\CERTIFICADOS'
-#path = 'C:\\Users\\gustavo.lima\\Desktop\\CERTIFICADOS'
-#path = 'C:\\Users\\Lucas.Oliveira\\Documents\\CarimbaCertificadoPython\\BN'
-
 
 def percorrer():
 
-	filearrays = { '.pdf':[],'-C.pdf':[], 'lotes':[]}
-	for root, dirs, files in os.walk(path): #aqui o 
-		for file in files:
-			filename, fileext = os.path.splitext(file)
-			if fileext in filearrays:
+	filearrays = { '.pdf':[],'-C.pdf':[], 'lotes':[]} # Cria um array de variaveis para armazenar documentos suportados.
+	for root, dirs, files in os.walk(path): #Funcao recursiva que percore as pastas apartir de um caminho.
+		for file in files: #Obtem um arquivo do array de arquivos encontrados na funcao os.walk
+			filename, fileext = os.path.splitext(file) #corto o caminho do arquivo por sua extencao. (ex: c:\pasta\arquivo.pdf fica filename =  c:\pasta\arquivo fileext = .pdf )
+			if fileext in filearrays: # Se a extencao encontrada tem o mesmo nome que uma das variaveis contidas em filearrays então.
 				#print (file.endswith('-C.pdf'))
-				if not(root.endswith('ORIGINAL')):
-					if not(file.endswith('-C.pdf')):
-						filearrays[fileext].append(os.path.join(root,file))
+				if not(root.endswith('ORIGINAL')): #Verifica se nao é a pasta original, isso é necessário para evitar que o sistema carimbe os arquivos que já foram carimbados.
+					if not(file.endswith('-C.pdf')): #O nome do arquivo não termina com a extensão '-C.pdf'
+						filearrays[fileext].append(os.path.join(root,file)) #Grava na variavel filearrays com extensão correspondente.
 					else:
-						filearrays['-C.pdf'].append(os.path.join(root,file))
-	return filearrays
+						filearrays['-C.pdf'].append(os.path.join(root,file)) #Grava na variavel filearrays com extensão '-C.pdf'
+	return filearrays # Retorno a lista de filearrays categorizados por extensao .pdf ou -C.pdf e seus respectivos nomes na variavel lote.
 
 def main():
+	# Utilizado para contar as execucoes de pesquisa de novos arquivos para carimbar.
 	execucoes = 0
 	#PARA FICAR O TEMPO TODO VERIFICANDO POR NOVOS ARQUIVOS E CARIMBANDO
-	while True:
-		execucoes = execucoes + 1
-		print ('Execução {}'.format(str(execucoes)))
-		arquivoscarimbar=percorrer()
-		for arqcarimbar in arquivoscarimbar['.pdf']:
-			carimbaPDF(arqcarimbar,"Rastreabilidade", "Jatinox")
+	while True: # LOOP infinito para manter o programa sempre rodando.
+		execucoes = execucoes + 1 #INCREMENTA A EXECUCAO DO PROGRAMA.
+		print ('Execução {}'.format(str(execucoes))) # Exibi para o usuario o andamento do programa.
+		arquivoscarimbar = percorrer() #Carrega a listagem de arquivos para carimbar em um array
+		for arqcarimbar in arquivoscarimbar['.pdf']: #For para percorrer os arquivos que vao ser carimbados.
+			#carimbaPDF(arqcarimbar,"Numero", "Rastreabilidade") #Chamando funcao que carimba os documentos.
+			carimbaPDF(arqcarimbar,"NR","") #Chamando funcao que carimba os documentos.
 			try:
-				moverarquivo(arqcarimbar)
+				moverarquivo(arqcarimbar) # Chamando funcao que move o arquivo original para pasta correta.
 			except:
 				print ('O arquivo abaixo está sendo utilizado por outro usuário e será movido assim que fechar o arquivo')
 				print(arqcarimbar)
-		#carimbaPDF("BN\\18\\24XX\\BN18-2400.pdf","BN18-2400","Rastreabilidade","Jatinox")
-		#carimbaPDF("original.pdf", "BN10-202", "Rastreabilidade", "Jatinox ")
-		time.sleep(120) # AGUARDA 60 SEGUNDOS ANTES DE REINICIAR O PROGRAMA
+		time.sleep(60) # AGUARDA 60 SEGUNDOS ANTES DE REINICIAR O PROGRAMA
 
 if __name__ == "__main__":
-	main()
+	main() # AQUI COMECA O PROGRAMA.
